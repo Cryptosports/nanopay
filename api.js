37ffdc7d9ac4c19573d0b8cf353240e55d7faad7
Aman Singh <besoeasy@users.noreply.github.com>
2020-08-16 12:38:35 +0100

    Update api.js

--- api.js
+++ api.js
@@ -26,7 +26,11 @@ var main = mongoose.model(
 );
 
 const app = express();
-const node = process.env.nanonode;
+
+// use remote node or 
+// docker run --restart=unless-stopped -d -p 7075:7075/udp -p 7075:7075 -p [::1]:7076:7076 -p [::1]:7078:7078 nanocurrency/nano:latest
+
+const node = process.env.nanonode || 'http://localhost:7076';
 app.listen(process.env.PORT || 5000, '0.0.0.0');
 
 app.use(cors());
