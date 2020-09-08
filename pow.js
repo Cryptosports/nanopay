e5fb287dad748e9fb65e1773d968d0750491b6da
Aman Singh <besoeasy@users.noreply.github.com>
2020-09-08 23:24:14 +0100

    Update pow.js

--- pow.js
+++ pow.js
@@ -1,7 +1,7 @@
 const nanocurrency = require('nanocurrency');
 const axios = require('axios');
 
-var worknode = process.env.worknode || 'http://127.0.0.1:2883';
+var worknode = process.env.worknode;
 
 async function hybirdwork(blockblock) {
 	console.log('work requested : ' + blockblock);
@@ -26,13 +26,13 @@ async function hybirdwork(blockblock) {
 (async () => {
 	while (true) {
 		try {
-			await axios.get('https://nanopay.besoeasy.com/work/get').then(async function (resx) {
+			await axios.get('/work/get').then(async function (resx) {
 				blockHash = resx.data.result[0].blockHash;
 
 				if (blockHash) {
 					xx = await hybirdwork(blockHash);
 
-					await axios.get('https://nanopay.besoeasy.com/workset/' + blockHash + '/' + xx).then(async function (response2) {
+					await axios.get('/workset/' + blockHash + '/' + xx).then(async function (response2) {
 						console.log(response2.data);
 					});
 				}
