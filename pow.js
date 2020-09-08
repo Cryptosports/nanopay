3957bc554a464f210a5030907dd6d6294ee99083
Aman Singh <besoeasy@users.noreply.github.com>
2020-09-08 23:57:23 +0100

    Update pow.js

--- pow.js
+++ pow.js
@@ -26,13 +26,12 @@ async function hybirdwork(blockblock) {
 (async () => {
 	while (true) {
 		try {
-			await axios.get('work/get').then(async function (resx) {
+			await axios.get('https://nanopay.besoeasy.com/work/get').then(async function (resx) {
 				blockHash = resx.data.result[0].blockHash;
 
 				if (blockHash) {
 					xx = await hybirdwork(blockHash);
-
-					await axios.get('workset/' + blockHash + '/' + xx).then(async function (response2) {
+					await axios.get('https://nanopay.besoeasy.com/workset/' + blockHash + '/' + xx).then(async function (response2) {
 						console.log(response2.data);
 					});
 				}
